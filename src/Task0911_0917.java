a435fb98b0c33f913fd7a009ee264ac0b7826b97
jackchen <wb.chen@outlook.com>
2017-09-17 19:18:00 +0800

    clean up

--- src/Task0911_0917.java
+++ src/Task0911_0917.java
@@ -6,7 +6,6 @@ public class Task0911_0917 {
 
     public int[] ParallelArrayGenerate(){
         int[] container = new int[1000];
-        Random rand = new Random();
 
         // 使用Java并行ForEach生成数组，样本为1000的情况下慢于普通的for循环
         IntStream.range(0, 1000).parallel().forEach(i -> {
